82a375ec49875fa83825ecc9216299cca87a554f
Gabe <33893811+Gabefire@users.noreply.github.com>
2023-06-11 10:39:22 -0500

    working on add education button

--- src/App.js
+++ src/App.js
@@ -3,18 +3,75 @@ import Heading from "./components/heading";
 import Education from "./components/education";
 import Experience from "./components/experience";
 import "./styles/index.css";
+import uniqid from "uniqid";
 
 class App extends React.Component {
   constructor() {
     super();
+    this.boundedAddEducation = this.addEducation.bind(this);
+    this.state = {
+      education: {
+        schoolName: "",
+        titleOfStudy: "",
+        date: "",
+        id: "",
+      },
+      educationArray: [
+        {
+          schoolName: "",
+          titleOfStudy: "",
+          date: "",
+          id: uniqid(),
+        },
+      ],
+    };
   }
+
+  addEducation(e) {
+    e.preventDefault();
+    const educationSection = document.querySelectorAll(".education");
+    console.log(educationSection);
+    educationSection.forEach((section) => {
+      const schoolName = section.childNodes[0].childNodes[1].value;
+      const titleOfStudy = section.childNodes[1].childNodes[1].value;
+      const date = section.childNodes[2].childNodes[1].value;
+      const id = section.id;
+      this.setState({
+        education: {
+          schoolName: schoolName,
+          titleOfStudy: titleOfStudy,
+          date: date,
+          id: id,
+        },
+      });
+      this.setState({
+        educationArray: [...this.state.educationArray, this.state.education],
+      });
+    });
+    this.setState({
+      education: {
+        schoolName: "",
+        titleOfStudy: "",
+        date: "",
+        id: uniqid(),
+      },
+      educationArray: [...this.state.educationArray, this.state.education],
+    });
+  }
+
   render() {
     return (
       <form className="App">
         <h1>CV Application</h1>
         <Heading />
-        <Education />
+        <section className="education-container">
+          <Education educationArray={this.state.educationArray} />
+          <button className="add-education-btn" onClick={this.addEducation}>
+            Add Education
+          </button>
+        </section>
         <Experience />
+        <button className="submit-form-btn">Submit Form</button>
       </form>
     );
   }
--- src/components/education.js
+++ src/components/education.js
@@ -2,34 +2,48 @@ import React from "react";
 import "../styles/education.css";
 
 export default class Education extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   render() {
     return (
-      <div id="education">
-        <label htmlFor="school-name">
-          School Name:
-          <input placeholder="Enter School Name" name="school-name"></input>
-        </label>
-        <label htmlFor="title-of-study">
-          Title of Study:
-          <input
-            placeholder="Enter Title of Study"
-            name="title-of-study"
-          ></input>
-        </label>
-        <label htmlFor="date-of-study">
-          Date of Study:
-          <input
-            type="text"
-            placeholder="Enter Date of Study"
-            name="date-of-study"
-          ></input>
-        </label>
-        <button className="add-education-btn">Add Education</button>
-      </div>
+      <>
+        {this.props.educationArray.map((education) => {
+          return (
+            <div className="education" id={education.id}>
+              <label htmlFor="school-name" className="school-name-label">
+                School Name:
+                <input
+                  defaultValue={education.schoolName}
+                  className="school-name"
+                  placeholder="Enter School Name"
+                  name="school-name"
+                ></input>
+              </label>
+              <label htmlFor="title-of-study">
+                Title of Study:
+                <input
+                  defaultValue={education.titleOfStudy}
+                  className="title-of-study"
+                  placeholder="Enter Title of Study"
+                  name="title-of-study"
+                ></input>
+              </label>
+              <label htmlFor="date-of-study">
+                Date of Study:
+                <input
+                  defaultValue={education.date}
+                  className="date-of-study"
+                  type="text"
+                  placeholder="Enter Date of Study"
+                  name="date-of-study"
+                ></input>
+              </label>
+            </div>
+          );
+        })}
+      </>
     );
   }
 }
