e019a0b3d4a9e7418026c72db93b471018b3dba0
Gabe <33893811+Gabefire@users.noreply.github.com>
2023-06-15 20:16:05 -0500

    made App into a funcitional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "./components/heading";
 import Education from "./components/education";
 import Experience from "./components/experience";
@@ -6,122 +6,51 @@ import Form from "./components/form";
 import "./styles/index.css";
 import uniqid from "uniqid";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.boundedSwitchViews = this.switchViews.bind(this);
-    this.boundedAddEducation = this.addEducation.bind(this);
-    this.boundedDeleteEducation = this.deleteEducation.bind(this);
-    this.boundedAddExperience = this.addExperience.bind(this);
-    this.boundedDeleteExperience = this.deleteExperience.bind(this);
-    this.boundedSubmitForm = this.submitForm.bind(this);
-    this.state = {
-      showContent: false,
-      displayForm: true,
-      education: {
-        schoolName: null,
-        titleOfStudy: null,
-        date: null,
-        key: null,
-      },
+const App = () => {
+  const [content, setContent] = useState(false);
+  const [form, setForm] = useState(true);
+  const [educationArray, setEducationArray] = useState([
+    {
+      schoolName: "School of Technology",
+      titleOfStudy: "Bachelors of Engineering",
+      date: "2013",
+      key: uniqid(),
+    },
+  ]);
+  const [experienceArray, setExperienceArray] = useState([
+    {
+      companyName: "Mortgage Company",
+      positionTitle: "Loan Coordinator",
+      dates: "2019-present",
+      task: "Provide excellent customer service and solve problems",
+      key: uniqid(),
+    },
+  ]);
+  const [heading, setHeading] = useState({
+    firstName: "Gabriel",
+    lastName: "Underwood",
+    email: "fake-email@gmail.com",
+    phoneNumber: "323-878-4538",
+    key: uniqid(),
+  });
 
-      educationArray: [
-        {
-          schoolName: "School of Technology",
-          titleOfStudy: "Bachelors of Engineering",
-          date: "2013",
-          key: uniqid(),
-        },
-      ],
+  const switchViews = () => {
+    setContent(true);
+    setForm(false);
+  };
 
-      experience: {
-        companyName: null,
-        positionTitle: null,
-        dates: null,
-        task: null,
-        key: uniqid(),
-      },
-
-      experienceArray: [
-        {
-          companyName: "Mortgage Company",
-          positionTitle: "Loan Coordinator",
-          dates: "2019-present",
-          task: "Provide excellent customer service and solve problems",
-          key: uniqid(),
-        },
-      ],
-
-      heading: {
-        firstName: "Gabriel",
-        lastName: "Underwood",
-        email: "fake-email@gmail.com",
-        phoneNumber: "323-878-4538",
-        key: uniqid(),
-      },
-    };
-  }
-
-  switchViews() {
-    this.setState({
-      showContent: !this.state.showContent,
-      displayForm: !this.state.displayForm,
-    });
-  }
-
-  showContent() {
+  const showContent = () => {
     return (
       <Form
-        heading={this.state.heading}
-        educationArray={this.state.educationArray}
-        experienceArray={this.state.experienceArray}
-        switchView={this.boundedSwitchViews}
+        heading={heading}
+        educationArray={educationArray}
+        experienceArray={experienceArray}
+        switchView={switchViews}
       />
     );
-  }
+  };
 
-  showForm() {
-    return (
-      <form key={uniqid()}>
-        <h2 className="input-title">Heading:</h2>
-        <Heading heading={this.state.heading} />
-        <h2 className="input-title">Education:</h2>
-        <section className="education-container">
-          <Education
-            educationArray={this.state.educationArray}
-            deleteEducation={this.boundedDeleteEducation}
-          />
-          <button
-            className="add-btn form-btn"
-            onClick={this.boundedAddEducation}
-          >
-            Add Education
-          </button>
-        </section>
-        <h2 className="input-title">Experience:</h2>
-        <section className="experience-container">
-          <Experience
-            experienceArray={this.state.experienceArray}
-            deleteExperience={this.boundedDeleteExperience}
-          />
-          <button
-            className="add-btn form-btn"
-            onClick={this.boundedAddExperience}
-          >
-            Add Experience
-          </button>
-        </section>
-        <button
-          className="submit-btn form-btn"
-          onClick={this.boundedSubmitForm}
-        >
-          Submit Form
-        </button>
-      </form>
-    );
-  }
-
-  addEducation(e) {
+  const addEducation = (e) => {
     e.preventDefault();
     const educationSection = document.querySelectorAll(".education");
     const educationArray = [];
@@ -147,13 +76,23 @@ class App extends React.Component {
 
     educationArray.push(educationObject);
 
-    this.setState({
-      educationArray: educationArray,
-    });
-    console.log(this.state.educationArray);
-  }
+    setEducationArray(educationArray);
+  };
 
-  addExperience(e) {
+  const deleteEducation = (e) => {
+    e.preventDefault();
+    let targetKey = e.target.id;
+    targetKey = targetKey.split("-")[1];
+    const index = educationArray.findIndex((education) => {
+      if (education.key === targetKey) {
+        return true;
+      }
+      return false;
+    });
+    setEducationArray(educationArray.filter((_, i) => i !== index));
+  };
+
+  const addExperience = (e) => {
     e.preventDefault();
     const experienceSection = document.querySelectorAll(".experience");
     const experienceArray = [];
@@ -183,55 +122,36 @@ class App extends React.Component {
 
     experienceArray.push(experienceObject);
 
-    this.setState({
-      experienceArray: experienceArray,
-    });
-  }
+    setEducationArray(experienceArray);
+  };
 
-  deleteEducation(e) {
+  const deleteExperience = (e) => {
     e.preventDefault();
     let targetKey = e.target.id;
     targetKey = targetKey.split("-")[1];
-    const index = this.state.educationArray.findIndex((education) => {
-      if (education.key === targetKey) {
-        return true;
-      }
-      return false;
-    });
-    this.setState({
-      educationArray: this.state.educationArray.filter((_, i) => i !== index),
-    });
-  }
-
-  deleteExperience(e) {
-    e.preventDefault();
-    let targetKey = e.target.id;
-    targetKey = targetKey.split("-")[1];
-    const index = this.state.experienceArray.findIndex((experience) => {
+    const index = experienceArray.findIndex((experience) => {
       if (experience.key === targetKey) {
         return true;
       }
       return false;
     });
-    this.setState({
-      experienceArray: this.state.experienceArray.filter((_, i) => i !== index),
-    });
-  }
 
-  submitForm(e) {
+    setExperienceArray(experienceArray.filter((_, i) => i !== index));
+  };
+
+  const submitForm = (e) => {
     e.preventDefault();
     const headingSection = document.querySelector(".heading");
     const firstName = headingSection.childNodes[0].childNodes[1].value;
     const lastName = headingSection.childNodes[1].childNodes[1].value;
     const email = headingSection.childNodes[2].childNodes[1].value;
     const phoneNumber = headingSection.childNodes[3].childNodes[1].value;
-    this.setState({
-      heading: {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        phoneNumber: phoneNumber,
-      },
+
+    setHeading({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      phoneNumber: phoneNumber,
     });
 
     const experienceSection = document.querySelectorAll(".experience");
@@ -251,9 +171,8 @@ class App extends React.Component {
       };
       experienceArray.push(experienceObject);
     });
-    this.setState({
-      experienceArray: experienceArray,
-    });
+
+    setExperienceArray(experienceArray);
 
     const educationSection = document.querySelectorAll(".education");
     const educationArray = [];
@@ -270,24 +189,53 @@ class App extends React.Component {
       };
       educationArray.push(educationObject);
     });
-    this.setState({
-      educationArray: educationArray,
-    });
-    this.setState({
-      displayForm: false,
-      showContent: true,
-    });
-  }
 
-  render() {
+    setEducationArray(educationArray);
+
+    setForm(false);
+
+    setContent(true);
+  };
+
+  const showForm = () => {
     return (
-      <div className="App">
-        <h1 className="title">CV Application</h1>
-        {this.state.displayForm ? this.showForm() : null}
-        {this.state.showContent ? this.showContent() : null}
-      </div>
+      <form key={uniqid()}>
+        <h2 className="input-title">Heading:</h2>
+        <Heading heading={heading} />
+        <h2 className="input-title">Education:</h2>
+        <section className="education-container">
+          <Education
+            educationArray={educationArray}
+            deleteEducation={deleteEducation}
+          />
+          <button className="add-btn form-btn" onClick={addEducation}>
+            Add Education
+          </button>
+        </section>
+        <h2 className="input-title">Experience:</h2>
+        <section className="experience-container">
+          <Experience
+            experienceArray={experienceArray}
+            deleteExperience={deleteExperience}
+          />
+          <button className="add-btn form-btn" onClick={addExperience}>
+            Add Experience
+          </button>
+        </section>
+        <button className="submit-btn form-btn" onClick={submitForm}>
+          Submit Form
+        </button>
+      </form>
     );
-  }
-}
+  };
+
+  return (
+    <div className="App">
+      <h1 className="title">CV Application</h1>
+      {form ? showForm() : null}
+      {content ? showContent() : null}
+    </div>
+  );
+};
 
 export default App;
