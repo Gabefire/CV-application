5e2430e0769016894ba05c8b91deba3f820dcaca
Gabe <33893811+Gabefire@users.noreply.github.com>
2023-06-15 20:31:04 -0500

    made experience into a functional component

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,6 +1,73 @@
 import React from "react";
 import "../styles/experience.css";
 
+const Experience = ({ experienceArray, deleteExperience }) => {
+  const addDeleteButton = (key) => {
+    return (
+      <button
+        className="delete-btn form-btn"
+        id={`btn-${key}`}
+        onClick={deleteExperience}
+      >
+        Delete Experience
+      </button>
+    );
+  };
+
+  return (
+    <>
+      {experienceArray.map((experience, index) => {
+        return (
+          <div className="experience" id={experience.key} key={experience.key}>
+            <label htmlFor={`company-name ${index}`}>
+              Company Name:
+              <input
+                placeholder="Enter Company Name"
+                name="company-name"
+                defaultValue={experience.companyName}
+                id={`company-name ${index}`}
+              ></input>
+            </label>
+            <label htmlFor={`position-title ${index}`}>
+              Title:
+              <input
+                placeholder="Enter Title"
+                name="position-title"
+                defaultValue={experience.positionTitle}
+                id={`position-title ${index}`}
+              ></input>
+            </label>
+            <label htmlFor={`dates ${index}`}>
+              Date Range:
+              <input
+                type="text"
+                placeholder="Enter Dates yyyy-yyyy"
+                name="dates"
+                defaultValue={experience.dates}
+                id={`dates ${index}`}
+              ></input>
+            </label>
+            <label htmlFor={`tasks ${index}`}>
+              Main Task:
+              <input
+                type="text"
+                placeholder="Enter Main Tasks"
+                name="task"
+                defaultValue={experience.task}
+                id={`tasks ${index}`}
+              ></input>
+            </label>
+            {index !== 0 ? addDeleteButton(experience.key) : null}
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Experience;
+
+/*
 export default class Experience extends React.Component {
   constructor(props) {
     super(props);
@@ -73,3 +140,4 @@ export default class Experience extends React.Component {
     );
   }
 }
+*/
