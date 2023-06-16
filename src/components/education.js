bd4dc49d920927ef2232b0af0f563b2041a562ea
Gabe <33893811+Gabefire@users.noreply.github.com>
2023-06-15 20:23:47 -0500

    made Education into a functional component

--- src/components/education.js
+++ src/components/education.js
@@ -1,65 +1,61 @@
 import React from "react";
 import "../styles/education.css";
 
-export default class Education extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  addDeleteButton(key) {
+const Education = ({ deleteEducation, educationArray }) => {
+  const addDeleteButton = (key) => {
     return (
       <button
         className="delete-btn form-btn"
         id={`btn-${key}`}
-        onClick={this.props.deleteEducation}
+        onClick={deleteEducation}
       >
         Delete Education
       </button>
     );
-  }
+  };
 
-  render() {
-    return (
-      <>
-        {this.props.educationArray.map((education, index) => {
-          return (
-            <div className="education" id={education.key} key={education.key}>
-              <label htmlFor={`school ${index}`} className="school-name-label">
-                School Name:
-                <input
-                  id={`school ${index}`}
-                  defaultValue={education.schoolName}
-                  className="school-name-input"
-                  placeholder="Enter School Name"
-                  name="school-name"
-                ></input>
-              </label>
-              <label htmlFor={`title-of-study ${index}`}>
-                Title of Study:
-                <input
-                  id={`title-of-study ${index}`}
-                  defaultValue={education.titleOfStudy}
-                  className="title-of-study"
-                  placeholder="Enter Title of Study"
-                  name="title-of-study"
-                ></input>
-              </label>
-              <label htmlFor={`date-of-study ${index}`}>
-                Date of Study:
-                <input
-                  id={`date-of-study ${index}`}
-                  defaultValue={education.date}
-                  className="date-of-study"
-                  type="text"
-                  placeholder="Enter Date of Study"
-                  name="date-of-study"
-                ></input>
-              </label>
-              {index !== 0 ? this.addDeleteButton(education.key) : null}
-            </div>
-          );
-        })}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {educationArray.map((education, index) => {
+        return (
+          <div className="education" id={education.key} key={education.key}>
+            <label htmlFor={`school ${index}`} className="school-name-label">
+              School Name:
+              <input
+                id={`school ${index}`}
+                defaultValue={education.schoolName}
+                className="school-name-input"
+                placeholder="Enter School Name"
+                name="school-name"
+              ></input>
+            </label>
+            <label htmlFor={`title-of-study ${index}`}>
+              Title of Study:
+              <input
+                id={`title-of-study ${index}`}
+                defaultValue={education.titleOfStudy}
+                className="title-of-study"
+                placeholder="Enter Title of Study"
+                name="title-of-study"
+              ></input>
+            </label>
+            <label htmlFor={`date-of-study ${index}`}>
+              Date of Study:
+              <input
+                id={`date-of-study ${index}`}
+                defaultValue={education.date}
+                className="date-of-study"
+                type="text"
+                placeholder="Enter Date of Study"
+                name="date-of-study"
+              ></input>
+            </label>
+            {index !== 0 ? addDeleteButton(education.key) : null}
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Education;
